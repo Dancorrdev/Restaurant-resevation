b99722b8f060058e5d7882ef6ff53208b5fd71d2
Danilo Cortes <dacortes63@misena.edu.co>
2022-04-17 22:19:19 -0500

    chore: se configuran los ajustes iniciales de el servidor y enrutamiento con las vistas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
-const morgan = require("morgan")
+const morgan = require("morgan");
 
 //Inicializaciones de dependencias
 const app = express();
 
 //Settings
 app.set("puerto", process.env.PORT || 4000);
-app.set("views", path.join(__dirname, "views"));
+//app.set("views", path.join(__dirname, "views"));
 
+//Middelwares
+app.use(morgan("dev"));
 
-//Mi
+//Rutas
+app.use(require("../src/routes/index.routes"));
+
+//Iniciando Server
+app.listen(app.get("puerto"), () => {
+  console.log("Server on port: ", app.get("puerto"));
+});
