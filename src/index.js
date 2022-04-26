6264da04b35285a7fc094595fda42a853a89b88b
Danilo Cortes <dacortes63@misena.edu.co>
2022-04-26 01:18:15 -0500

    chore: se realiza ejemplo para conexión de base de datos y configuracion del servidor
    en el archivo keys esta el objeto con los datos para la conexion con la base de datos y en el database esta la configuracion

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 
 //Inicializaciones de dependencias
 const app = express();
 
 //Settings
 app.set("puerto", process.env.PORT || 4000);
-//app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //Middelwares
 app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //Rutas
 app.use(require("../src/routes/index.routes"));
+app.use("/reservation", require("../src/routes/reservation.routes"));
 
 //Iniciando Server
 app.listen(app.get("puerto"), () => {
---
+++ src/keys.js
@@ -0,0 +1,11 @@
+
+/*  Clave para la conexion con la base de datos
+module.exports = {
+    database:{
+        host: 'localhost',
+        user: 'root',
+        password: '12345',
+        database: 'mydb'
+    }
+};
+*/
