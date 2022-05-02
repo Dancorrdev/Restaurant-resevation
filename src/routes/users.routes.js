9e3917c60318cf4929c663ee6f8e363833f78ffc
Danilo Cortes <dacortes63@misena.edu.co>
2022-05-02 15:49:12 -0500

    Chore: se realiza el enrutado de las rutas post para los usuarios

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/new", (req, res) =>{
-    res.render("users/create")
+router.get("/new", (req, res) => {
+  res.render("users/create");
+});
+
+router.post("/new", (req,res) =>{
+    res.redirect("/users")
 })
 
-router.get("/edit", (req, res) =>{
-    res.render("users/edit")
+router.get("/edit", (req, res) => {
+  res.render("users/edit");
+});
+
+router.post("/edit", (req, res) =>{
+    res.redirect("/users")
 })
 
 router.get("/", (req, res) => {
-    res.render("users/read")
+  res.render("users/read");
 });
 
 module.exports = router;
