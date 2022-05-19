8daa23d06f391f129fa83a77c51f7ca73e80a9f7
Danilo <dacortes63@misena.edu.co>
2022-05-19 07:38:26 -0500

    chore: se realiza el enrutamiento de nuevo password para la recuperacion de password

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,6 +5,10 @@ router.get("/password/reset", (req, res) => {
   res.render("queries/recoverPasswords");
 });
 
+router.get("/password/new", (req, res) => {
+  res.render("queries/newPassword");
+});
+
 router.get("/", (req, res) => {
   res.render("login");
 });
